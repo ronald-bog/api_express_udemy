83f89e8905e3414b8a9cf90d31a536387e60d56c
Ronald Espitia <ronald79@gmail.com>
2025-03-13 19:38:16 -0500

    para subir a github

--- app.js
+++ app.js
@@ -2,15 +2,17 @@ const express = require('express');
 
 const app = express();
 const port = 3000;
+const cors = require('cors');
+app.use(cors());
 
 let clientes = [
-    {nombre: "JazzTel", cif: "A12345678", direccion: "Gran Vía, 22", localidad: "Barcelona"},
-    {nombre: "Orange", cif: "A87654321", direccion: "Príncipe Pío, 40", localidad: "Bilbao"},
-    {nombre: "Movistar", cif: "A44445555", direccion: "Castellana, 100", localidad: "Madrid"},
+    { nombre: "JazzTel", cif: "A12345678", direccion: "Gran Vía, 22", localidad: "Barcelona" },
+    { nombre: "Orange", cif: "A87654321", direccion: "Príncipe Pío, 40", localidad: "Bilbao" },
+    { nombre: "Movistar", cif: "A44445555", direccion: "Castellana, 100", localidad: "Madrid" },
 ]
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.status(200).send(clientes);
@@ -20,7 +22,7 @@ app.get('/:cif', (req, res) => {
     let cliente = clientes.find(elem => {
         return elem.cif === req.params.cif;
     })
-    if(cliente === undefined) {
+    if (cliente === undefined) {
         return res.status(404).json({
             mensaje: 'No se encontró ningún cliente con ese CIF'
         })
@@ -32,7 +34,7 @@ app.get('/:cif', (req, res) => {
 
 
 app.post('/', (req, res) => {
-    if(req.body === undefined) {
+    if (req.body === undefined) {
         return res.status(400).json({
             mensaje: 'Datos de cliente obligatorios'
         })
@@ -45,12 +47,12 @@ app.post('/', (req, res) => {
 })
 
 app.put('/:cif', (req, res) => {
-    if(req.body === undefined) {
+    if (req.body === undefined) {
         return res.status(400).json({
             mensaje: 'Datos de cliente a actualizar obligatorios'
         })
     }
-    if(req.params.cif === undefined) {
+    if (req.params.cif === undefined) {
         return res.status(400).json({
             mensaje: 'CIF del cliente a actualizar obligatorio'
         })
@@ -58,18 +60,18 @@ app.put('/:cif', (req, res) => {
     const posicion = clientes.findIndex(elem => {
         return elem.cif === req.params.cif;
     })
-    if(posicion < 0) {
+    if (posicion < 0) {
         return res.status(404).json({
             mensaje: 'Cliente no encontrado'
         })
     }
-    if(req.body.nombre !== undefined) {
+    if (req.body.nombre !== undefined) {
         clientes[posicion].nombre = req.body.nombre;
     }
-    if(req.body.direccion !== undefined) {
+    if (req.body.direccion !== undefined) {
         clientes[posicion].direccion = req.body.direccion;
     }
-    if(req.body.localidad !== undefined) {
+    if (req.body.localidad !== undefined) {
         clientes[posicion].localidad = req.body.localidad;
     }
     res.status(201).json({
@@ -79,7 +81,7 @@ app.put('/:cif', (req, res) => {
 })
 
 app.delete('/:cif', (req, res) => {
-    if(req.params.cif === undefined) {
+    if (req.params.cif === undefined) {
         return res.status(400).json({
             mensaje: 'CIF del cliente a actualizar obligatorio'
         })
@@ -87,7 +89,7 @@ app.delete('/:cif', (req, res) => {
     const posicion = clientes.findIndex(elem => {
         return elem.cif === req.params.cif;
     })
-    if(posicion < 0) {
+    if (posicion < 0) {
         return res.status(404).json({
             mensaje: 'Cliente no encontrado'
         })
